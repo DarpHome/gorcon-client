c1b80d63298a3444937e4ece919eaf1e056227ae
MCausc78 <mcausc78@gmail.com>
2023-10-12 15:09:27 +0300

    v1.3.0

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
 module github.com/DarpHome/gorcon-client
 
-go 1.20
+go 1.21
 
-require github.com/DarpHome/gorcon v1.1.0
+toolchain go1.21.1
+
+require github.com/DarpHome/gorcon v1.3.0
+
+require golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
--- main.go
+++ main.go
@@ -17,12 +17,26 @@ func PanicIf(err error) {
 }
 
 func main() {
-	address := flag.String("address", "127.0.0.1:25575", "Address of RCON server")
-	password := flag.String("password", "mypassw0rd", "RCON password")
+	addressp := flag.String("address", "127.0.0.1:25575", "Address of RCON server")
+	passwordp := flag.String("password", "mypassw0rd", "RCON password")
 	flag.Parse()
-	client := gorcon.NewRCONClient(*address)
-	PanicIf(client.Login(*password))
-	defer client.Close()
+	address := *addressp
+	password := *passwordp
+	/* rs := gorcon.NewRCONServer(nil)
+	rs.Check(gorcon.ForPassword(password)).OnCommand(func(ctx *gorcon.RCONCommandContext) {
+		ctx.Reply("hey, " + ctx.Command)
+	}).OnLogged(func(ctx *gorcon.RCONContext) {
+		fmt.Printf("Someone logged, IP: %s\n", ctx.Connection.RemoteAddr().String())
+	}).OnExit(func(ctx *gorcon.RCONContext) {
+		fmt.Printf("Someone quit: %s\n", ctx.Connection.RemoteAddr().String())
+	}).OnError(func(cctx *gorcon.RCONCommandContext, ctx *gorcon.RCONContext, err error) {
+		fmt.Fprintf(os.Stderr, "Cctx: %v\nCtx: %v\nError: %v", cctx, ctx, err)
+	})
+	go rs.Run(":25585")
+	time.Sleep(1 * time.Second) */
+	client := gorcon.NewRCONClient()
+	PanicIf(client.Connect(address))
+	PanicIf(client.Login(password))
 	fmt.Printf("Logged with request ID %d!\n", client.RequestID)
 	reader := bufio.NewReader(os.Stdin)
 	for {
@@ -35,6 +49,11 @@ func main() {
 		}
 		res, err := client.SendCommand(cmd)
 		PanicIf(err)
+		if !strings.HasSuffix(res, "\n") {
+			res += "\n"
+		}
 		fmt.Print(res)
 	}
+	client.Close()
+	// rs.Close()
 }
